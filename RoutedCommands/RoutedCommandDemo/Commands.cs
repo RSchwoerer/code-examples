3da2aa7eef8448630d605c0487600481901b5b75
Ryan Schwoerer <rschwoerer@veeco.com>
2016-06-24 10:14:09 -0500

    added other RoutedCommands demo

---
+++ RoutedCommands/RoutedCommandDemo/Commands.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Input;
+
+namespace RoutedCommandDemo
+{
+    public static class Commands
+    {
+        /// <summary>
+        /// Represents the Foo feature of the program.
+        /// </summary>
+        public static readonly RoutedCommand Foo = new RoutedCommand();
+    }
+}
---
+++ RoutedCommands/RoutedCommandDemo/Window1.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RoutedCommandDemo
+{
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        void Foo_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            // The Window gets to determine if the Foo 
+            // command can execute at this time.
+            e.CanExecute = true;
+        }
+
+        void Foo_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            // The Window executes the command logic when the user wants to Foo.
+            MessageBox.Show("The Window is Fooing...");
+        }
+    }
+}
